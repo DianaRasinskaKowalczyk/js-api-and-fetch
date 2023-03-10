c690e62fdb4f143b39514108e33ae1f21d620d70
Diana Rasińska-Kowalczyk <diana.rasinska@gmail.com>
2023-03-10 13:20:53 +0100

    Try to insert data to html but it doesn't work

--- src/js/client.js
+++ src/js/client.js
@@ -3,21 +3,54 @@ import ExcursionsAPI from "./ExcursionsAPI";
 console.log("client");
 
 document.addEventListener("DOMContentLoaded", init);
+const tripsUrl = "http://localhost:3000/excursions";
 
 function init() {
 	console.log("init");
+	loadTrips();
 }
 
 function loadTrips() {
-	fetch("http://localost:3000/excursions")
+	fetch(tripsUrl)
 		.then(resp => {
 			if (resp.ok) {
 				return resp.json();
 			}
 			return Promise.reject(resp);
 		})
-		.then(resp => console.log(resp))
+		.then(data => console.log(data))
+		.then(data => console.log(Array.isArray(data)))
+
+		.then(data => insertTripsToHtml(data))
 		.catch(err => console.error(err));
 }
 
-loadTrips();
+function insertTripsToHtml(tripsData) {
+	const tripPrototype = document.querySelector(".excursions__item--prototype");
+	const tripsWrapper = document.querySelector(".panel__excursions");
+
+	tripsData.forEach(trip => {
+		const singleTrip = tripPrototype.cloneNode(true);
+		singleTrip.classList.remove("excursions__item--prototype");
+		console.log(singleTrip);
+
+		const tripTitle = tripPrototype.querySelector(".excursions__title");
+		const tripDescription = tripPrototype.querySelector(
+			".excursions__description"
+		);
+
+		const adultPrice = tripPrototype.querySelector(".adultPrice");
+		const childPrice = tripPrototype.querySelector(".childPrice");
+
+		console.log(adultPrice, childPrice);
+
+		tripTitle.innerText = trip.name;
+
+		console.log(trip.name);
+		tripDescription.innerText = trip.description;
+		adultPrice.innerText = trip.adultPrice;
+		childPrice.innerText = trip.childPrice;
+
+		tripsWrapper.appendChild(singleTrip);
+	});
+}
