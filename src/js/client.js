980c3c503eff11011790d18f949073e2f5523fca
Diana Rasińska-Kowalczyk <diana.rasinska@gmail.com>
2023-03-10 11:21:04 +0100

    Add loadtrips

--- src/js/client.js
+++ src/js/client.js
@@ -1,5 +1,23 @@
-import './../css/client.css';
+import "./../css/client.css";
+import ExcursionsAPI from "./ExcursionsAPI";
+console.log("client");
 
-import ExcursionsAPI from './ExcursionsAPI';
+document.addEventListener("DOMContentLoaded", init);
 
-console.log('client');
+function init() {
+	console.log("init");
+}
+
+function loadTrips() {
+	fetch("http://localost:3000/excursions")
+		.then(resp => {
+			if (resp.ok) {
+				return resp.json();
+			}
+			return Promise.reject(resp);
+		})
+		.then(resp => console.log(resp))
+		.catch(err => console.error(err));
+}
+
+loadTrips();
